f7ee35f4e8742faad8e22c4e0e4b1564d4478ac8
Cida Castello <56411540+cidacastello@users.noreply.github.com>
2020-06-17 19:14:49 -0300

    Ex04_Velocidade_MUV
    Resolução do exercício 4

---
+++ Exercicios/Ex04_Velocidade_MUV/Ex04_Velocidade_MUV/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Ex04_Velocidade_MUV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTempo = new System.Windows.Forms.Label();
+            this.txtTempo = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.txtVelocidade = new System.Windows.Forms.TextBox();
+            this.lblVelocidade = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTempo
+            // 
+            this.lblTempo.AutoSize = true;
+            this.lblTempo.Location = new System.Drawing.Point(20, 20);
+            this.lblTempo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTempo.Name = "lblTempo";
+            this.lblTempo.Size = new System.Drawing.Size(98, 25);
+            this.lblTempo.TabIndex = 0;
+            this.lblTempo.Text = "Tempo(s)";
+            // 
+            // txtTempo
+            // 
+            this.txtTempo.Location = new System.Drawing.Point(168, 22);
+            this.txtTempo.Name = "txtTempo";
+            this.txtTempo.Size = new System.Drawing.Size(100, 30);
+            this.txtTempo.TabIndex = 0;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(12, 130);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(128, 33);
+            this.btnCalcular.TabIndex = 2;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(146, 130);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(128, 33);
+            this.btnLimpar.TabIndex = 3;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // txtVelocidade
+            // 
+            this.txtVelocidade.Location = new System.Drawing.Point(168, 76);
+            this.txtVelocidade.Name = "txtVelocidade";
+            this.txtVelocidade.Size = new System.Drawing.Size(100, 30);
+            this.txtVelocidade.TabIndex = 1;
+            // 
+            // lblVelocidade
+            // 
+            this.lblVelocidade.AutoSize = true;
+            this.lblVelocidade.Location = new System.Drawing.Point(36, 76);
+            this.lblVelocidade.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblVelocidade.Name = "lblVelocidade";
+            this.lblVelocidade.Size = new System.Drawing.Size(110, 25);
+            this.lblVelocidade.TabIndex = 0;
+            this.lblVelocidade.Text = "Velocidade";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(313, 188);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtVelocidade);
+            this.Controls.Add(this.txtTempo);
+            this.Controls.Add(this.lblVelocidade);
+            this.Controls.Add(this.lblTempo);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Cálculo da Velocidade";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTempo;
+        private System.Windows.Forms.TextBox txtTempo;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.TextBox txtVelocidade;
+        private System.Windows.Forms.Label lblVelocidade;
+    }
+}
+
---
+++ Exercicios/Ex04_Velocidade_MUV/Ex04_Velocidade_MUV/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex04_Velocidade_MUV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            //declaração das variáveis
+            double velocidadeInicial = 10, aceleracao = 3;
+            double tempo, velocidadeFinal;
+
+            //entrada de dados com validação
+            if(double.TryParse(txtTempo.Text, out tempo) == false){
+                MessageBox.Show("Valor inválido", "Velocidade", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtTempo.Clear();
+                txtTempo.Focus();
+                return; //interrompe o processamento
+            }
+            
+            //processamento
+            velocidadeFinal = velocidadeInicial + aceleracao * tempo;
+
+            txtVelocidade.Text = velocidadeFinal.ToString();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtVelocidade.ResetText();
+            txtTempo.ResetText();
+            txtTempo.Focus();
+        }
+    }
+}
